e903035588df756e86b95745665148bf13a329ea
OriginalByteMe <noahrijkaard@gmail.com>
2023-06-29 16:15:11 +0800

    update requirements + app config

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import beam
 
 app = beam.App(
     name="cutout_generator",
-    cpu=1,
-    gpu="T4",
+    cpu=4,
+    gpu="A10G",
     memory="32Gi",
-    python_version="python3.8",
+    python_version="python3.10",
     python_packages="requirements.txt",
     commands=["apt-get update && apt-get install -y ffmpeg"],
 )
@@ -14,3 +14,4 @@ app.Mount.PersistentVolume(path="./uploaded_images", name="images")
 app.Mount.PersistentVolume(path="./masks", name="masks")
 app.Mount.PersistentVolume(path="./cutouts", name="cutouts")
 app.Mount.PersistentVolume(path="./models", name="models")
+app.Mount.PersistentVolume(path="./svg", name="svgs")
