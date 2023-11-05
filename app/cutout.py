93f22524b4ed6d0b98886ab1ed04b92572d9568b
OriginalByteMe <noahrijkaard@gmail.com>
2023-11-05 23:51:05 +0800

    modify cutout to use envionrmental variables

--- app/cutout.py
+++ app/cutout.py
@@ -1,13 +1,31 @@
 import cv2
 import numpy as np
 import os
+from s3_handler import Boto3Client
+from dino import Dino
+from segment import Segmenter
 
+HOME = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 
 class CutoutCreator:
-  def __init__(self, image_folder):
-    self.image_folder = image_folder
+  def __init__(self, grounding_dino_config_path, grounding_dino_checkpoint_path):
+    self.dino = Dino(classes=['person', 'nose', 'chair', 'shoe', 'ear', 'hat'],
+        box_threshold=0.35,
+        text_threshold=0.25,
+        model_config_path=grounding_dino_config_path,
+        model_checkpoint_path=grounding_dino_checkpoint_path)
+    self.s3 = Boto3Client()
 
-  def create_cutouts(self, image_name, masks, output_folder,bucket_name, s3):
+  def create_cutouts(self, image_name, sam_checkpoint_path):
+    
+    # Download image from s3
+    image_path = self.s3.download_from_s3(os.path.join(HOME, "data"), "cutout-image-store", f"images/{image_name}")
+    
+    image = cv2.imread(image_path)
+    segment = Segmenter(sam_encoder_version="vit_h", sam_checkpoint_path=sam_checkpoint_path)
+    detections = self.dino.predict(image)
+    
+    masks = segment.segment(image, detections.xyxy)
     # Load the image
     image_path = os.path.join(self.image_folder, image_name)
     for item in os.listdir(self.image_folder):
@@ -29,10 +47,10 @@ class CutoutCreator:
 
       # Save the cutout
       cutout_name = f"{image_name}_cutout_{i}.png"
-      cutout_path = os.path.join(output_folder, cutout_name)
+      cutout_path = os.path.join("cutouts", cutout_name)
       cv2.imwrite(cutout_path, cutout)
 
       # Upload the cutout to S3
       with open(cutout_path, "rb") as f:
-        s3.upload_to_s3(bucket_name, f.read(), f"cutouts/{image_name}/{cutout_name}")
+        self.s3.upload_to_s3(f.read(), f"cutouts/{image_name}/{cutout_name}")
 
