18daff06dde1e36113deb35bb3b605f1bd87e8f0
OriginalByteMe <noahrijkaard@gmail.com>
2023-11-05 23:51:30 +0800

    Simplify s3 inputs to have auto bucket

--- app/s3_handler.py
+++ app/s3_handler.py
@@ -12,27 +12,25 @@ class Boto3Client:
             region_name=os.environ["AWS_REGION"],
         )
 
-    def download_from_s3(self, save_path, bucket_name, key):
+    def download_from_s3(self, save_path, image_name):
         s3_client = boto3.client("s3")
-
-        file_name = key.split("/")[-1]
-        file_path = os.path.join(save_path, file_name)
+        file_path = os.path.join(save_path, image_name)
         try:
-            s3_client.download_file(bucket_name, key, file_path)
+            s3_client.download_file(os.environ["CUTOUT_BUCKET"], f"cutouts/{image_name}/cutout-{image_name}", file_path)
         except ClientError as e:
             print("BOTO error: ",e)
             return None
 
         return file_path
 
-    def upload_to_s3(self, bucket_name, file_body, key):
-        self.s3.put_object(Body=file_body, Bucket=bucket_name, Key=key)
+    def upload_to_s3(self, file_body, image_name):
+        self.s3.put_object(Body=file_body, Bucket=os.environ["CUTOUT_BUCKET"], Key=f"images/{image_name}")
 
-    def generate_presigned_url(self, bucket_name, key, expiration=3600):
+    def generate_presigned_url(self, image_name, expiration=3600):
         try:
             response = self.s3.generate_presigned_url(
                 "get_object",
-                Params={"Bucket": bucket_name, "Key": key},
+                Params={"Bucket": os.environ["CUTOUT_BUCKET"], "Key": f"cutouts/{image_name}/cutout-{image_name}"},
                 ExpiresIn=expiration,
             )
         except ClientError as e:
